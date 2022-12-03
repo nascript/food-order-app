c1577209908aade65121e41832fbbc0b42e482ea
nascript <nascodefy@gmail.com>
2022-12-03 15:40:55 +0700

    header and router vue

---
+++ src/views/FoodView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar msg="Welcome to Your Vue.js App" />
+    <div class="container mt-10">
+      <h2>List of Foods</h2>
+    </div>
+  </div>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  name: 'Foods',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar msg="Welcome to Your Vue.js App" />
+    <div class="container mt-50">
+      <h2>Welcome to Nas Restaurant</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Navbar,
+  },
+}
+</script>
